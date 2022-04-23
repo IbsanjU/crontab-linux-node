20af0e02acca217160e611ce1a63684ced8ad9aa
IbsanjU <bharath2668@gmail.com>
2022-04-23 08:05:12 +0530

    relative path in crontab

--- test.sh
+++ test.sh
@@ -4,8 +4,11 @@ source /home/ibsanju/.profile
 
 TIMESTAMP=`date "+%Y-%m-%d %H:%M:%S"`
 echo "$TIMESTAMP running test script. $TEST_ENV - this is a test from profile"
-/usr/bin/node /home/ibsanju/Documents/github/crontab-linux-node/index.js &>> /home/ibsanju/Documents/github/crontab-linux-node/node_log.log &disown
+node /home/ibsanju/Documents/github/crontab-linux-node/index.js &>> /home/ibsanju/Documents/github/crontab-linux-node/node_log.log &disown
 
 # crontab -e
 # Add absolute path for log and executing shell
 # */1 * * * * /home/ibsanju/Documents/github/crontab-linux-node/test.sh >> /home/ibsanju/Documents/github/crontab-linux-node/test.log
+
+# For relative path in crontab
+# PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin
